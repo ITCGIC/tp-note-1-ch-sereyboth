017c36bb2cccadd32c230cddb9859fe57bfb38bf
chamroeunsereyboth <ch.sereyboth@gmail.com>
2019-12-20 12:39:46 +0700

    Task1_Sereyboth

---
+++ src/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+    public static void main(String[] args){
+        System.out.println("**************************");
+        System.out.println("*                      * *");
+        System.out.println("*                   *    *");
+        System.out.println("*                *       *");
+        System.out.println("*             *          *");
+        System.out.println("*          *             *");
+        System.out.println("*       *                *");
+        System.out.println("*    *                   *");
+        System.out.println("* *                      *");
+        System.out.println("**************************");
+
+    }
+}
